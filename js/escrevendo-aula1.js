133e253a9a39561c42b9175c41a623edc5ee0aea
wanderson <wanderson.santos.was120@gmail.com>
2022-06-29 20:04:44 -0300

    atualização foto de fundo

--- js/escrevendo-aula1.js
+++ js/escrevendo-aula1.js
@@ -11,7 +11,7 @@ var button = document.querySelector("button");
 button.onclick = function () {
   var nome = prompt("Qual é o comando para iniciar o git?");
   if ((nome = "git init")) {
-    alert("O" + nome + ", serve para iniciar o git.Parabéns você acertou!");
+    alert("O" + nome + ", serve para iniciar o git. Parabéns você acertou!");
   }
   window.location="aula2.html";
 };
